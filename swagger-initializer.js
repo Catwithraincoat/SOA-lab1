3a33297eb92bef5797cb3c2af14eec0e715097f9
Anastasia Pavlova <99503656+Catwithraincoat@users.noreply.github.com>
2024-10-14 18:42:44 +0300

    Add files via upload

---
+++ swagger-initializer.js
@@ -0,0 +1,22 @@
+window.onload = function() {
+  //<editor-fold desc="Changeable Configuration Block">
+
+  // the following lines will be replaced by docker/configurator, when it runs in a docker-container
+  window.ui = SwaggerUIBundle({
+     urls: [
+          { url: "./openapi1.yaml", name: "Collection Web Service" },
+          { url: "./openapi2.yaml", name: "Chapter Web Service" }
+        ],    dom_id: '#swagger-ui',
+    deepLinking: true,
+    presets: [
+      SwaggerUIBundle.presets.apis,
+      SwaggerUIStandalonePreset
+    ],
+    plugins: [
+      SwaggerUIBundle.plugins.DownloadUrl
+    ],
+    layout: "StandaloneLayout"
+  });
+
+  //</editor-fold>
+};
